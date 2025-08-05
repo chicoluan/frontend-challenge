68e39d7441a89341105f6b806e0db336d342816d
engluanoliv <engluanoliv@gmail.com>
2025-08-05 17:01:11 -0300

    formated

--- types/index.ts
+++ types/index.ts
@@ -1,22 +1,22 @@
 interface Condition {
-  questionId: string;
-  expectedValue: string;
-  action: 'show';
+  questionId: string
+  expectedValue: string
+  action: 'show'
 }
 
 interface Question {
-  id: string;
-  label: string;
-  type: string;
-  options?: { id: string; label: string }[];
-  conditions?: Condition[];
+  id: string
+  label: string
+  type: string
+  options?: { id: string; label: string }[]
+  conditions?: Condition[]
 }
 
 // biome-ignore lint/correctness/noUnusedVariables: <>
 interface FormSchema {
-  id: string;
-  title: string;
-  questions: Question[];
+  id: string
+  title: string
+  questions: Question[]
 }
 
 // export type FieldType = "text" | "select" | "checkbox";
--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,10 +1,10 @@
-import { createBrowserClient } from "@supabase/ssr";
+import { createBrowserClient } from '@supabase/ssr'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY
 
 if (!supabaseUrl || !supabaseKey) {
-	throw new Error("Missing Supabase environment variables");
+  throw new Error('Missing Supabase environment variables')
 }
 
-export const supabase = () => createBrowserClient(supabaseUrl, supabaseKey);
+export const supabase = () => createBrowserClient(supabaseUrl, supabaseKey)
