1e4a3d67aa54ff282536f641bca1613150936f83
engluanoliv <engluanoliv@gmail.com>
2025-08-05 15:55:59 -0300

    added zustand to state dialog

--- app/form/components/create-form/create-form.tsx
+++ app/form/components/create-form/create-form.tsx
@@ -1,6 +1,17 @@
+"use client";
 // biome-ignore assist/source/organizeImports: <>
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/separator";
+import { Textarea } from "@/components/ui/textarea";
 import { cn } from "@/lib/utils";
 import type { JSX } from "react";
 
@@ -12,12 +23,30 @@ export default function CreateForm({
 		<div className={cn("flex flex-col gap-6", className)} {...props}>
 			<Card>
 				<CardHeader>
-					<CardTitle>Novo formulário</CardTitle>
+					<CardTitle className="md:text-xl">Novo formulário</CardTitle>
+					<CardDescription>
+						Escreva o titulo para adicionar as perguntas
+					</CardDescription>
 				</CardHeader>
-                <CardContent>
-                    
-                    <Button type="button" variant='link'>Adicionar Pergunta</Button>
-                </CardContent>
+				<Separator />
+				<CardContent className="flex flex-col justify-end gap-3">
+					<div className="flex flex-col gap-2">
+						<Label>Titulo</Label>
+						<Input />
+					</div>
+					<div className="flex flex-col gap-2">
+						<Label>Descrição</Label>
+						<Textarea />
+					</div>
+					<Button
+						type="button"
+						variant="outline"
+						className="self-end hover:cursor-pointer"
+						disabled
+					>
+						Adicionar Pergunta
+					</Button>
+				</CardContent>
 			</Card>
 		</div>
 	);
--- schemas/schema.ts
+++ schemas/schema.ts
@@ -2,11 +2,40 @@ import { z } from "zod";
 
 export const questionSchema = z.object({
 	id: z.uuid(),
-})
+	id_formulario: z.uuid(),
+	titulo: z.string(),
+	codigo: z.string(),
+	orientacao_resposta: z.string(),
+	ordem: z.number(),
+	obrigatoria: z.boolean(),
+	sub_pergunta: z.boolean(),
+	tipo_pergunta: z.enum([
+		"Sim_Não",
+		"multipla_escola",
+		"unica_escolha",
+		"texto_livre",
+		"Inteiro",
+		"Numero com duas casa decimais",
+	]),
+});
 
 export const formSchema = z.object({
 	id: z.uuid(),
 	titulo: z.string(),
 	descricao: z.string().optional(),
 	ordem: z.number(),
+});
+
+export const optionAnswerSchema = z.object({
+	id: z.uuid(),
+	id_pergunta: z.uuid(),
+	resposta: z.string(),
+	ordem: z.number(),
+	resposta_aberta: z.boolean(),
+});
+
+export const optionQuestionAnswerSchema = z.object({
+	id: z.uuid(),
+	id_opcao_resposta: z.uuid(),
+	id_pergunta: z.uuid(),
 });
